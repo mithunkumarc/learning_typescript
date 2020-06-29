14640a98200c8656c0730b5baf80555ae1a6b66e
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-29 20:33:21 +0530

    Update slice.ts

--- string/slice.ts
+++ string/slice.ts
@@ -1,3 +1,9 @@
 const p : string = "i love javascript";
 // slice(start_index, end_index) : returns sub string
 console.log(p.slice(2,6)); // love 
+
+// negetive index start at the end of the string
+// substring : negetive index
+// similar to pythong slicing [start: end : <interval, not available in js>]
+console.log(p.slice(-6)) // "script"
+console.log(p.slice(-16,-11)) // "love"
