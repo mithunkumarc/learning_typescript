ab7fdb2c9deca8d3c9bcb236b5604aed4833d740
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-29 21:04:48 +0530

    Create substring.ts

---
+++ string/substring.ts
@@ -0,0 +1,7 @@
+const p : string = "i love javascript";
+// substring(start_index, end_index)
+console.log(p.substring(7,17)) // javascript
+console.log(p.substring(11,17)) // script
+
+// negetive index not working
+console.log(p.substring(-11,-1))//
