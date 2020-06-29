aa0aba85e1f3b8816f5025b919a30623e0c92c9a
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-29 20:44:51 +0530

    Create substr.ts

---
+++ string/substr.ts
@@ -0,0 +1,6 @@
+// substr(from, how_many_characters)
+const p : string = "i love javascript";
+// substr(from, how_many_characters)
+console.log(p.substr(7, 10));// javascript
+// negetive index
+console.log(p.substr(-10, 10)) // javascript
