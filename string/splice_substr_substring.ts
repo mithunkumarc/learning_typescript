40f8fbd0a9ffc9beaf8648000ed6e9f8c08bc5dc
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-29 21:07:40 +0530

    Create splice_substr_substring.ts

---
+++ string/splice_substr_substring.ts
@@ -0,0 +1,4 @@
+all returns substring  
+1.  substring(start_index, end_index) : negetive index not working
+2.  substr(from_index, how_many_characters) : negetive index works
+3.  slice(start_index, end_index) :  : negetive index works
