665f552d7d355e180b4a88d248a0215806de693b
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-29 11:07:28 +0530

    Create custome_index.ts

---
+++ enum/custome_index.ts
@@ -0,0 +1,10 @@
+// an enum is a way of giving more friendly names to sets of numeric values.
+enum Color {
+    Red = 10, 
+    Green = 15,
+    Blue = 25
+}
+
+console.log(Color.Red); // 10
+console.log(Color.Green); // 15
+console.log(Color.Blue); // 25
