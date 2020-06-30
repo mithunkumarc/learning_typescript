cb721ac7e6b73615b777323e3aa40e109c99a243
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-30 18:41:11 +0530

    Create some.ts

---
+++ arrays/some.ts
@@ -0,0 +1,6 @@
+// some(function) : 
+// Determines whether the specified callback function 
+// returns true for any element of an array.
+const ar: number[] = [11,22,33,44,55,33];
+// one of the element is 44
+console.log(ar.some(e => e == 44))
