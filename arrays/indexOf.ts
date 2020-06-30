1db443406fe0379f10998ce8dd88e0613bfc8981
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-30 13:10:41 +0530

    Create indexOf.ts

---
+++ arrays/indexOf.ts
@@ -0,0 +1,4 @@
+//indexOf : returns index of element if exists else -1
+const ar: number[] = [11,22,33,44,55];
+console.log(ar.indexOf(22)); // index of 22 is 1
+console.log(ar.indexOf(67)); // 67 doesn't exists. so -1
