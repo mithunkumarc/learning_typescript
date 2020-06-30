82625cf4e51436c5eb679a5d133dadbbaa8a1bdd
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-30 12:31:17 +0530

    Create findIndex.ts

---
+++ arrays/findIndex.ts
@@ -0,0 +1,4 @@
+// search an element , if it is present , returns its index
+const ar: number[] = [11,22,33,44,55];
+const index = ar.findIndex(element => element == 33)
+console.log(index); // index of element 33 : 2
