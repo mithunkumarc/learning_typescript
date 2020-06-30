aea0a93d6a251c3b07b9c9e32de23b0530d7c87a
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-06-30 09:54:16 +0530

    Create concat.ts

---
+++ arrays/concat.ts
@@ -0,0 +1,11 @@
+// syntax : yourArray.concat(tobe_add_array)
+// creates new array by adding given elements
+// original array remain unaltered
+
+const ar : number[] = [1,2,3,4,5];
+
+console.log(ar.concat([8,9,8])); // [1, 2, 3, 4, 5, 8, 9, 8] // you can store in new variable
+
+console.log(ar); // [1, 2, 3, 4, 5]
+
+
