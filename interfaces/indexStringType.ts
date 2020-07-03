19668426769f19e0011edc1e397082919dbb9885
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-07-03 20:44:18 +0530

    Update indexStringType.ts

--- interfaces/indexStringType.ts
+++ interfaces/indexStringType.ts
@@ -3,6 +3,6 @@ class Veggie {
   [index: string]: number;
 }
 const v: Veggie = {"tomato":50, "onion":40, "lady's finger":25};
-console.log(v["tomato"]); // 50
+console.log(v["tomato"]); // 50  , same as console.log(v.onion)
 console.log(v["onion"]); // 40
 console.log(v["lady's finger"]); // 25
