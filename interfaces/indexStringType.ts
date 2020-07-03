e7cd0a57dd5ee36163e8b61418c77961e34c036a
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-07-03 20:36:54 +0530

    Create indexStringType.ts

---
+++ interfaces/indexStringType.ts
@@ -0,0 +1,8 @@
+// string as index
+class Veggie {
+  [index: string]: number;
+}
+const v: Veggie = {"tomato":50, "onion":40, "lady's finger":25};
+console.log(v["tomato"]); // 50
+console.log(v["onion"]); // 40
+console.log(v["lady's finger"]); // 25
