f93a870613068c55d84ee5cec5c782fefb988a90
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-07-13 21:39:44 +0530

    Update genericInterface.ts

--- generics/genericInterface.ts
+++ generics/genericInterface.ts
@@ -10,8 +10,17 @@ interface genericInterface {
 
 
 let myIdentity: genericInterface = identity;
-
- 
-
-
 console.log(myIdentity("hello world"))
+
+/* or above generic interface can be rewritten as
+
+interface GenericIdentityFn<T> {
+    (arg: T): T;
+}
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let myIdentity: GenericIdentityFn<number> = identity;
+*/
