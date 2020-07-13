02cf4d02b1ab25816fe8c2b269a27cfac27a8e0a
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-07-13 10:39:06 +0530

    Update overloading.ts

--- functions/overloading.ts
+++ functions/overloading.ts
@@ -1,10 +1,10 @@
 // declare function with overloaded parameters
 // input combination must be number and string : return string
-function demo(x: number, y: string): string;
+function demo(x: number, y: string): string;    // lets say rule 1
 // input : string, boolean , output : number
-function demo(x: string, y: boolean): number;
+function demo(x: string, y: boolean): number;  // lets say rule 2
 
-// x : either number or string
+// should accept rule 1 (input : number/string, output : string), rule 2 (input: string/boolean , output : number)
 function demo(x: any, y: any): any {
   //should return number/string , dependns type of param combination
 }
